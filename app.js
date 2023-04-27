223f35c8bf42ada50cb546db9da3838b98a4d9be
maupineyro <argmau357@gmail.com>
2023-04-27 00:14:31 -0300

    solucionado error json.parse unexpected end of JSON input

---
+++ app.js
@@ -0,0 +1,8 @@
+//Entregable 3
+
+//Importo express y la class ProductManager
+import express from "express";
+import ProductManager from "./src/ProductManager.js";
+
+
+const app = express ();
---
+++ src/ProductManager.js
@@ -0,0 +1,137 @@
+//importar fileSystem
+import { promises as fs } from 'fs';
+
+//Creo la clase ProductManager
+export default class ProductManager {
+    constructor (){
+        this.products = [];
+        this.path = "./dbProducts.txt";
+        this.incrementalId = 0
+    }
+
+    //método addProduct
+    addProduct = async ( title, description, price, thumbnail, code, stock) => {
+
+    this.incrementalId++;
+
+    let newProduct = {//estructura del producto a agregar
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+        id: this.incrementalId,
+   }
+    
+    this.products.push (newProduct);//uso this.products para almacenar los diferentes push que genere el addProduct
+    
+    try{ //write    
+        let Write = await fs.writeFile (this.path, JSON.stringify(this.products, null, 2));          
+    }
+    catch (err){
+        console.log("error al agregar o crear el archivo");
+    }
+}//cierra el addProduct 
+
+    //"sub-método" readProducts (con un delay para evitar problemas con la escritura del txt / json)
+    readProducts = async () => {
+        return new Promise((resolve, reject) => {
+          setTimeout(async () => {
+            try {
+              let readProductsResponse = await fs.readFile(this.path, "utf-8");
+              let readProductsParsed = JSON.parse(readProductsResponse);
+              resolve(readProductsParsed);
+            } catch (err) {
+              reject(err);
+            }
+          }, 750); // Espera 0.75 seg para leer el archivo, para esperar que la escritura del fs.writefile finalice - No devuelva undefined y json unexpected end error
+        });
+      };
+
+    //método getProducts
+    getProducts = async () => {
+        try {
+          const readAsync = await this.readProducts();
+          console.log(readAsync);
+        } catch (err) {
+          console.error("no se pueden obtener los productos desde el archivo path",err);
+        }
+      };
+
+    //método getProductById
+    getProductById =async (id) => {
+        let readAsyncFindId= await this.readProducts();
+        let findID = readAsyncFindId.find((product) => product.id === id);
+        if (findID){
+            console.log ("producto encontrado por ID es:")
+            console.log (findID);
+        } else {
+            console.log ("Producto no encontrado");
+        }
+    }
+
+    //método deleteProduct
+    deleteProduct = async (id) => {
+        let readProductsForDelete= await this.readProducts();
+        const filterProducts = readProductsForDelete.filter ((products) => products.id !== id)
+        console.log (`el product Id elegido para borrar es ${id}`);
+        await fs.writeFile (this.path, JSON.stringify(filterProducts));
+        console.log ("producto eliminado, chequear dbProducts.txt");
+        
+    }
+
+    //método updateProduct
+    updateProduct = async (id, newProps) =>{
+        let readProductsForUpdate = await this.readProducts();
+        let productToUpdate = readProductsForUpdate.find ((product) => product.id === id);
+        console.log (`el product id elegido para modificar es ${id}`, productToUpdate);
+
+        if (productToUpdate){
+            if (newProps.title){
+                productToUpdate.title = newProps.title
+            };
+            if (newProps.description){
+                productToUpdate.description = newProps.description
+            }
+            if (newProps.price){
+                productToUpdate.price = newProps.price
+            }
+            if (newProps.thumbnail){
+                productToUpdate.thumbnail= newProps.thumbnail
+            }
+            if (newProps.code){
+                productToUpdate.code = newProps.code
+            }
+             if (newProps.stock){
+                productToUpdate.stock = newProps.stock
+            }
+            if (newProps.id && newProps.id !== productToUpdate.id){
+                console.log ("el Id de un producto no puede modificarse con este método")
+            }
+
+            await fs.writeFile (this.path, JSON.stringify(readProductsForUpdate));
+            console.log (`Producto actualizado: ${JSON.stringify(productToUpdate)}`)
+        } else {
+            console.log (`No se encontró el producto Id: ${id}`)
+        }
+
+        
+
+    }
+
+
+
+} //cierra la class ProductManager
+
+/////////////////////////////////////////////////////////////////////////////////
+const productManager = new ProductManager; // Instanciar const productManager //
+///////////////////////////////////////////////////////////////////////////////
+
+productManager.addProduct ("título 1","description 1", 100,"imagen 1", "abc121",25);
+productManager.addProduct ("título 2","description 2", 100,"imagen 2", "abc122",25);
+productManager.addProduct ("título 3","description 3", 100,"imagen 3", "abc123",25);
+
+
+productManager.getProducts()
+
